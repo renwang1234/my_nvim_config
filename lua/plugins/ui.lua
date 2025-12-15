f9aea67e466de0085fbd984783cf1b3384b6d6d6
renwang1234 <164132457+renwang1234@users.noreply.github.com>
2025-12-15 09:33:10 +0800

    Update ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -136,26 +136,6 @@ return {
   },
 
   {
-    "NvChad/nvim-colorizer.lua",
-    event = "VeryLazy",
-    opts = {
-      user_default_options = {
-        names = false,
-        RRGGBB = true,
-        RRGGBBAA = true,
-        rgb_fn = true,
-        hsl_fn = true,
-        css = true,
-        css_fn = true,
-        tailwind = false,
-      },
-    },
-    config = function(_, opts)
-      require("colorizer").setup(opts)
-    end,
-  },
-
-  {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
@@ -186,45 +166,6 @@ return {
       "rcarriga/nvim-notify",
     }
   },
-
-  {
-    "folke/trouble.nvim",
-    opts = {},
-    cmd = "Trouble",
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-      },
-      {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-      },
-      {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-      },
-    },
-  },
-
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
@@ -245,4 +186,5 @@ return {
     version = '*'
   },
 }
+
 
