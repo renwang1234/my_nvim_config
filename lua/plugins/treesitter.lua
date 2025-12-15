cdf0421fc60e8bd120e8cb208bac688620086665
renwang1234 <164132457+renwang1234@users.noreply.github.com>
2025-12-16 00:22:58 +0800

    Refactor treesitter.lua configuration format

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,11 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   main = "nvim-treesitter.configs",
   opts = {
-    auto_install = false,
     ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "json", "python", "css", "html", "javascript", "typescript" },
+    auto_install = false,
     sync_install = false,
     highlight = { enable = true, additional_vim_regex_highlighting = false },
     indent = { enable = true },
   },
 }
+
