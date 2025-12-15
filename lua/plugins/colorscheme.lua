6786a9f89e69ef588f91c9d1cba68d746946d031
renwang1234 <164132457+renwang1234@users.noreply.github.com>
2025-12-15 09:07:09 +0800

    Change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,34 +4,14 @@ return {
   priority = 1000,
 
   config = function()
-    local g, cmd, api = vim.g, vim.cmd, vim.api
-
-    g.everforest_background = "hard"                                             -- "hard", "medium", "soft"
-    g.everforest_float_style = "dim"                                             --"bright","dim"
-    g.everforest_ui_contrast = "high"
-    g.everforest_enable_italic = true
-    g.everforest_enable_italic_comment = true
+    local g, cmd = vim.g, vim.cmd
     g.everforest_better_performance = true
-    g.everforest_dim_inactive_windows = true
-    g.everforest_spell_foreground = "colored"
+    g:everforest_diagnostic_line_highlight = 1
     g.everforest_transparent_background = true
-    g.everforest_sign_column_background = "none"
-    g.everforest_inlay_hints_background = "none"                                 --"dimmed"
-    g.everforest_diagnostic_virtual_text = "highlighted"                         --"grey","colored","highlighted"
-    g.everforest_nvimtree_transparent_background = true
-
     cmd("colorscheme everforest")
-    cmd("highlight LineNr guifg=#757567")                                        --#55544a #42423a #757576
-    cmd("highlight Comment guifg=#fad7f2")
+    cmd("highlight LineNr gui=#757567")
     cmd("highlight CursorNr guifg=#b8ccbe")
-    cmd("highlight CursorLine guibg=#5b6266")                                    --#6e777d
-    cmd("highlight ColorColumn guibg=#5b6266")                                   --#ccccaf #dedeb4 #829e9e
-
-    -- For completion menus like blink.cmp.
-    --vim.api.nvim_set_hl(0, 'Pmenu', {})
-    -- For regular floating windows.
-    api.nvim_set_hl(0, 'NvimTreeNormalFloat', {})
-    api.nvim_set_hl(0, 'NvimTreeNormalFloatBorder', {})
-
+    cmd("highlight CursorLine guibg=#5b6266")
+    cmd("highlight ColorColumn gui g=#5b6266")
   end
 }
