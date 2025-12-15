9d77caf80204a1d59996c882683248d5e851a08f
renwang1234 <164132457+renwang1234@users.noreply.github.com>
2025-12-15 23:52:56 +0800

    Update Treesitter configuration with new events and languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
   main = "nvim-treesitter.configs",
   opts = {
     auto_install = false,
-    ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+    ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "json", "python", "css", "html", "javascript", "typescript" },
     sync_install = false,
-    highlight = { enable = true },
+    highlight = { enable = true, additional_vim_regex_highlighting = false },
     indent = { enable = true },
   },
 }
