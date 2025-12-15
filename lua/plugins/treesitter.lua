8be1dca1a37eaedc9d7f690c9616388735e140fc
renwang1234 <164132457+renwang1234@users.noreply.github.com>
2025-12-15 09:29:44 +0800

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,62 +1,11 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    main = "nvim-treesitter.configs",
-    opts = {
-      auto_install = false,
-      ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
+  "nvim-treesitter/nvim-treesitter",
+  main = "nvim-treesitter.configs",
+  opts = {
+    auto_install = false,
+    ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
   },
-
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("treesitter-context").setup({
-        enable = true,
-        max_lines = 5,
-        min_window_height = 0,
-        line_numbers = true,
-        multiline_threshold = 3,
-        trim_scope = "outer",
-      })
-    end,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    main = "nvim-treesitter.configs",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-            },
-          },
-        },
-      })
-    end,
-  }
 }
